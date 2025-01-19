7bfa21208af66dc1370f1cec437980c7d6407051
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2025-01-19 21:29:39 +0900

    알고리즘 문제 풀이

---
+++ 250119/정올 1329번 별삼각형 3.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define SIZE 1000
+#define mod 1000000007
+#define MAX 4000001
+#define INF 1000000007
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+void solve();
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0 || n < 1 || n >100) {
+        cout << "INPUT ERROR!";
+        return;
+    }
+
+    for (int i = 0; i < n / 2 + 1; i++) {
+        for (int j = n / 2 - i; j < n / 2; j++)
+            cout << " ";
+
+        for (int j = n / 2 - i * 2; j < n / 2 + 1; j++)
+            cout << "*";
+
+        cout << '\n';
+    }
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = i + 1; j < n / 2; j++)
+            cout << " ";
+
+        for (int j = 2 + 2 * i; j < n; j++)
+            cout << "*";
+
+        cout << '\n';
+    }
+}
