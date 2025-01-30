9aff9589923e1e0a4f4b8a2dee2ed4a801a52024
chanki <115077587+chanki00@users.noreply.github.com>
2025-01-30 22:12:30 +0900

    Delete 250128/BOJ/Main2941.java

--- 250128/BOJ/Main2941.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main2941 {
-    public static void main(String[] args) throws Exception {
-    }
-}
