29f1fef6b902efd97377622736ee496adf037c3e
sshsgh <ssbaejs@gmail.com>
2025-02-06 23:27:57 +0900

    Create Main.java

---
+++ 250206/백준/15664. N과 M (10)/Main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static HashSet<ArrayList<Integer>> compareArr = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input1 = br.readLine().split(" ");
+        int n = Integer.parseInt(input1[0]);
+        int m = Integer.parseInt(input1[1]);
+        
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int i = 0;
+        while (st.hasMoreTokens()) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        	i++;
+        }
+        Arrays.sort(arr);
+        int idx = 0;
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        recursive(n, m, idx, arr, resultArr, bw);
+        bw.flush();
+        bw.close();
+    }
+    private static void recursive(int n, int m, int idx, int[] arr, ArrayList<Integer> resultArr, BufferedWriter bw) throws IOException {
+		// basis
+		if (resultArr.size() == m) {
+			ArrayList<Integer> tempList = new ArrayList<>();
+			for (int i = 0 ; i < m ; i++) {
+				tempList.add(resultArr.get(i));
+			}
+			if (compareArr.contains(tempList)) {
+				return;
+			} else {
+				compareArr.add(tempList);
+			}
+			for (int i = 0 ; i < m ; i++) {
+				bw.write(resultArr.get(i) + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		if (idx == n) {
+			return ;
+		}
+		
+		// inductive
+		resultArr.add(arr[idx]);
+		recursive(n, m, idx+1, arr, resultArr, bw);
+		resultArr.remove(resultArr.size()-1);
+		recursive(n, m, idx+1, arr, resultArr, bw);
+		
+	}
+}
