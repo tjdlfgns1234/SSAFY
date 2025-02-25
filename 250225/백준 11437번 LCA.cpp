108fcd80b6b1d6302d9aef16f867f942475fdb4a
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2025-02-25 15:32:40 +0900

    LCA 풀이

--- 250225/백준 11437번 LCA.cpp
+++ 250225/백준 11437번 LCA.cpp
@@ -1,32 +1,55 @@
-#include <iostream>
-#include <algorithm>
-#include <list>
-#include <vector>
-#include <queue>
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define SIZE 1000
+#define mod 1000000007
 #define MAX 50000
-using namespace std;
-vector<vector<int>> graph(MAX);
-int Lca(int v, int w);
-int d[MAX + 1] = { 0 };
-int parent[MAX + 1] = { 0 };
-bool visit[MAX + 1] = { false };
-void bfs(); // d 확인
+#define INF 1000000007
 
-int main(void)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+vector<vector<int>> g(MAX);
+int d[MAX + 1] = { 0 };
+int p[MAX + 1] = { 0 };
+bool vit[MAX + 1] = { false };
+
+void solve();
+int Lca(int x, int y);
+void bfs();
+
+int main()
 {
-	ios::sync_with_stdio(NULL);
-	cout.tie(NULL), cin.tie(NULL);
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+void solve() {
 
 	int n, m;
 	cin >> n;
 	d[1] = 1;
 
+	int x, y;
 	for (int i = 0; i < n - 1; i++)
 	{
-		int v, w;
-		cin >> v >> w;
-		graph[v].push_back(w);
-		graph[w].push_back(v);
+		cin >> x >> y;
+		g[x].push_back(y);
+		g[y].push_back(x);
 	}
 
 	bfs();
@@ -34,35 +57,34 @@ int main(void)
 
 	for (int i = 0; i < m; i++)
 	{
-		int v, w;
-		cin >> v >> w;
-		cout << Lca(v, w) << '\n';
+		cin >> x >> y;
+		cout << Lca(x, y) << '\n';
 	}
 
-	return 0;
+
+    // cout << ans;
+   //  cout << "#" << cnt++ << ' ' << ans << '\n';
 }
-void bfs() // d 확인
-{
+void bfs() {
 	queue <int> q;
-	visit[1] = 1; // 루트는 무조건 1번
+	vit[1] = true;
 	q.push(1);
-	while (!q.empty())
-	{
+
+
+	// 깊이 및 부모 기록
+	while (!q.empty()) {
 		int curr = q.front();
 		q.pop();
-		
-		for (auto i : graph[curr]) 
-			if (!visit[i]) 
-				q.push(i), visit[i] = true, d[i] = d[curr] + 1, parent[i] = curr;
+
+		for (auto& i : g[curr])
+			if (!vit[i])
+				q.push(i), vit[i] = true, p[i] = curr, d[i] = d[curr] + 1;
 	}
 }
-int Lca(int v, int w)
-{
-	// 더 깊은 것만 선택
-	if(d[v] > d[w]) swap(v,w);
-
-	while (d[v] != d[w])w = parent[w];
-
-	while (v != w)
-		v = parent[v], w = parent[w];
-	return w;
+int Lca(int x, int y) {
+	if (d[x] < d[y]) swap(x, y);
+	// 더 깊은 것을 선택
+	while (d[x] != d[y]) x = p[x];
+	while (x != y) x = p[x], y = p[y];
+	return x;
+}
