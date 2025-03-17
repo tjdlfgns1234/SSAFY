2b8d748d9d5abad9ad812077295e098e02b24701
grayson1999 <bbt1250912@gmail.com>
2025-03-17 17:45:16 +0900

    [BOJ-9251]: LCS

---
+++ 250317/BOJ_LCS.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_LCS {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = " "+br.readLine();
+        String str2 = " "+br.readLine();
+
+        int[][] mat = new int[str1.length()][str2.length()];
+        for(int i = 1; i < str1.length(); i++){
+            for(int j = 1; j < str2.length(); j++){
+                if(str1.charAt(i) == str2.charAt(j)){
+                    mat[i][j] = mat[i-1][j-1] + 1;
+                }else{
+                    mat[i][j] = Math.max(mat[i][j-1], mat[i-1][j]);
+                }
+            }
+        }
+        
+        System.out.println(mat[str1.length()-1][str2.length()-1]);
+        
+
+    }
+}
