2bee9b094a2ad0f81b896656de495fc860ef38e8
wntjdals0412 <wntjdals0412@gmail.com>
2025-04-08 11:24:13 +0900

    LIS (nlogn)

---
+++ 250408/백준 14003번 가장 긴 증가하는 부분 수열 5.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define k 256
+#define MOD 127
+#define INF 987654321
+#define ll long long
+using namespace std;
+
+int n;
+void solve();
+
+int dp[1'000'001];
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(), cout.tie();
+
+	solve();
+
+	return 0;
+}
+void solve() {
+	cin >> n;
+
+	vector<int> arr(n);
+	vector<int> ans;
+
+	for (auto& i : arr)
+		cin >> i;
+
+	//for (auto& i : arr)
+	//	cout << i.second << '\n';
+
+	for (int i = 0; i < n;i++) {
+		if(ans.empty())
+			ans.push_back(arr[i]), dp[0] = 0;
+		else if (arr[i] < ans[0])
+			ans[0] = arr[i], dp[i] = 0;
+		else if (ans.back() < arr[i])
+			ans.push_back(arr[i]), dp[i] = ans.size() - 1;
+		else {
+			auto it = (upper_bound(all(ans), arr[i] - 1));
+			*it = arr[i];
+			dp[i] = it-ans.begin();
+		}
+	}
+	int idx = ans.size() -1;
+	cout << ans.size() << '\n';
+
+	stack<int> s;
+
+	//cout << "-------------------------\n";
+	for (int i = n-1; i >=0 ; i--) {
+		// cout << dp[i] << " ";
+		if (dp[i] == idx)
+			s.push(arr[i]), idx--;
+	}
+		
+	while (!s.empty())
+		cout << s.top() << " ", s.pop();
+
+}
