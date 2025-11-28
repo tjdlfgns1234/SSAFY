ac855dbb52092d0f74c64185b11d40046ee9c489
서일훈 <tjdlfgns1234@naver.com>
2025-11-28 10:05:52 +0900

    Add : S1, Greedy

---
+++ 25년 11월/251128/백준 17615번 볼 모으기.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define INF 2e9
+#define sum(a) (a*(a+1)/2)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+int chk(const char target);
+
+int n;
+string s;
+
+/*
+	S1, Greedy
+
+	4가지 경우만 보면 되지만 증명이 쉽지 않다.
+*/
+
+int main()
+{
+	ios::sync_with_stdio(NULL);	
+	cin.tie(NULL), std::cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int t = 1;
+	cin >> t;
+
+	while (t--)
+		solve();
+
+	return 0;
+}
+void solve() {
+	cin >> n >> s;
+
+	int ans = INF;
+	ans = min(chk('B'), chk('R'));
+
+	cout << ans << '\n';
+}
+int chk(const char target) {
+	int cur = 0, cnt = 0;
+	int ret = INF;
+	// B R로 만드는 경우
+	for (int i = 0; i < n; i++)
+		if (s[i] == target)
+			cur++, cnt++;
+		else
+			cnt = 0;
+	cur -= cnt;
+	ret = min(ret, cur);
+
+	cur = 0, cnt = 0;
+
+	// R B로 만드는 경우
+	for (int i = n - 1; i >= 0; i--)
+		if (s[i] == target)
+			cur++, cnt++;
+		else
+			cnt = 0;
+	cur -= cnt;
+	ret = min(ret, cur);
+
+	return ret;
+}
